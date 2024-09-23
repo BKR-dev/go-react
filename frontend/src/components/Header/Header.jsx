6ed96d75a7a209b287432cb3a30c73d3a2ebf85a
Lionel <business@bkr.codes>
2024-09-23 16:20:49 +0200

    getting better

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,39 @@
+import style from "./Header.module.scss";
+
+function Header() {
+  return (
+    <header className={style["header"]}>
+      <nav>
+        <ul>
+          <logo>
+            <a href="/">
+              <img src="./fake-logo.png"></img>
+            </a>
+          </logo>
+          <li>
+            <nav-item>
+              <a href="#Cluster">Cluster</a>
+            </nav-item>
+          </li>
+          <li>
+            <nav-item>
+              <a href="#Deployments">Deployments</a>
+            </nav-item>
+          </li>
+          <li>
+            <nav-item>
+              <a href="#Applications">Applications</a>
+            </nav-item>
+          </li>
+          <li>
+            <nav-item>
+              <a href="#Networks">Networks</a>
+            </nav-item>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,21 +1,20 @@
-import React from 'react';
-import '../styles/Layout.css';
-import Header from './Header'
-import Footer from './Footer'
-import SideBar from './SideBar/SideBar'
+import React from "react";
+import "../styles/Layout.css";
+import Header from "./Header/Header";
+import Footer from "./Footer";
+import SideBar from "./SideBar/SideBar";
 
-
-const Layout = ({children }) => {
+const Layout = ({ children }) => {
   return (
-  <div>
-      <Header/>
-    <main>
-      <SideBar/>
-      {children}
-    </main>
-      <Footer/>
-  </div>    
-  )
-}
+    <div>
+      <Header />
+      <main>
+        <SideBar />
+        {children}
+      </main>
+      <Footer />
+    </div>
+  );
+};
 
-export default Layout
+export default Layout;
